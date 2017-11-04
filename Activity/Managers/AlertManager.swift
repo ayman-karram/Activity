6ba2fb2a576eac404285f3781799118ce7fcd424
ayman-karram <ayman.karram@gamil.com>
2017-11-04 23:25:43 +0200

    Created Alert Manager

---
+++ Activity/Managers/AlertManager.swift
@@ -0,0 +1,19 @@
+//
+//  AlertManager.swift
+//  Activity
+//
+//  Created by Ayman  on 11/4/17.
+//  Copyright © 2017 Ayman . All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AlertManager {
+    
+    class func getAlerWithTitle (title : String , message : String) -> UIAlertController {
+        let aler = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        aler.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+        return aler
+    }
+}
