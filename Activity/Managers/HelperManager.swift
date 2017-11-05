50340beb2ef7e96b3ffe1fd89597417df47d77fb
ayman-karram <ayman.karram@gamil.com>
2017-11-05 19:55:26 +0200

    Added HelperManager with  navigateToLoginController to use when user get verification account message

---
+++ Activity/Managers/HelperManager.swift
@@ -0,0 +1,22 @@
+//
+//  HelperManager.swift
+//  Activity
+//
+//  Created by Ayman  on 11/5/17.
+//  Copyright © 2017 Ayman . All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HelperManager {
+    
+    class func navigateToLoginController() {
+         let appDelegate = UIApplication.shared.delegate as! AppDelegate
+         let registartionStoryBoard = UIStoryboard(name: "Main", bundle: nil)
+         let landingPageNavigationController = registartionStoryBoard.instantiateViewController(withIdentifier: "LandingPageNV") as! UINavigationController
+        let landingPageVC = landingPageNavigationController.viewControllers.first as! LandingPageViewController
+        landingPageVC.navigateToLoginController = true
+        appDelegate.window?.rootViewController = landingPageNavigationController
+    }
+}
