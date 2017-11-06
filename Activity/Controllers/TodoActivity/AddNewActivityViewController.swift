6ba342a15abd30df577b12cc9bd49d7bddaff969
ayman-karram <ayman.karram@gamil.com>
2017-11-06 13:40:17 +0200

    Create AddNewActivityViewController

---
+++ Activity/Controllers/TodoActivity/AddNewActivityViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddNewActivityViewController.swift
+//  Activity
+//
+//  Created by Ayman  on 11/6/17.
+//  Copyright © 2017 Ayman . All rights reserved.
+//
+
+import UIKit
+
+class AddNewActivityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
